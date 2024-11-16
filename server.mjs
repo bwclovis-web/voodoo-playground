972338c57de5eab1f27dedf369863e2230887cf6
Brian <bwclovis.web@gmail.com>
2024-11-16 09:07:43 -0500

    add path clean

--- server.mjs
+++ server.mjs
@@ -45,6 +45,17 @@ app.use(prom({
   metricsPath: "/metrics"
 }))
 
+// Clean paths with trailing slashes
+app.use((req, res, next) => {
+  if (req.path.endsWith('/') && req.path.length > 1) {
+    const query = req.url.slice(req.path.length)
+    const safePath = req.path.slice(0, -1).replace(/\/+/g, '/')
+    res.redirect(301, safePath + query)
+  } else {
+    next()
+  }
+})
+
 // handle SSR requests
 app.all(
   "*",
