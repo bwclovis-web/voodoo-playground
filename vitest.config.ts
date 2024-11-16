fc66ed9efedb7e7387d461b690f591ffbc81ed95
Brian <bwclovis.web@gmail.com>
2024-11-16 08:44:52 -0500

    u[date coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,30 @@
 
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vitest/config"
+import { configDefaults, defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    coverage: {
+      exclude: [
+        ...configDefaults.exclude,
+        "./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "./stories/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "./generator/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "./build/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "*.config.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "./.storybook/**/*.ts",
+        "./.eslintrc.cjs",
+        "./server.mjs",
+        "./app/entry.client.tsx",
+        "./app/entry.server.tsx",
+        "./app/root.tsx",
+        "./app/routes/*.tsx",
+        "./prisma/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
+      ]
+    },
     environment: "happy-dom",
     globals: true,
     include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
