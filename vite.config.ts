8c5af7d24283f69fb2568c1e68e4f353627dc849
Brian <bwclovis.web@gmail.com>
2024-11-17 07:41:58 -0500

    update routes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import { envOnlyMacros } from "vite-env-only"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { flatRoutes } from 'remix-flat-routes'
 
 declare module "@remix-run/node" {
   interface Future {
@@ -14,12 +15,16 @@ export default defineConfig({
   plugins: [
     envOnlyMacros(),
     remix({
+      ignoredRouteFiles: ['**/.*'],
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes)
+      },
       future: {
         v3_fetcherPersist: true,
         v3_lazyRouteDiscovery: true,
         v3_relativeSplatPath: true,
         v3_singleFetch: true,
-        v3_throwAbortReason: true
+        v3_throwAbortReason: true,
       }
     }),
     tsconfigPaths()
